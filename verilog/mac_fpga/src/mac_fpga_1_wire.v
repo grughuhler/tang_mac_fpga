e26be162200fd898276c9cc46d90d3d24b884d00
Grug Huhler <grug.huhler@gmail.com>
2024-04-24 21:41:57 -0500

    add 1-wire versions of code

--- verilog/mac_fpga/src/mac_fpga_1_wire.v
+++ verilog/mac_fpga/src/mac_fpga_1_wire.v
@@ -1,18 +1,18 @@
 /* mac_fpga top level module
  *
  * License SPDX: BSD-2-Clause
- *
- * This version uses two unidirectional signals.  Use mac_command
+ * 
+ * This version uses a single bidirectional signal.  Use mac_command1
  * with it.  Note that some pin choices differ between the versions.
+ * 
  */
 
 module mac_fpga
    (
-    input wire [1:0]  cmd,
+    input wire [1:0]  cmd, // use pull-down
     input wire 	      clk,
-    input wire 	      din,
-    output wire       dout
-    //output wire [5:0] leds
+    inout wire        iop,
+    output wire [5:0] leds
     );
 
    parameter WIDTH = 16;
@@ -27,6 +27,9 @@ module mac_fpga
    wire 		c_ena;
    wire 		c_clk;
    wire 		p_ld;
+   wire         din;  // from RPI to FPGA
+   wire         dout; // from FPGA to RPI
+   wire         to_pi_ena;
    
    sipo_sr #(.WIDTH(WIDTH)) a
      (.clk(ab_clk),
@@ -64,12 +67,18 @@ module mac_fpga
    // arithmetic  
    assign sum_out = a_out*b_out + sum_in;
 
+   // Tri-state I/O control  Enable iop as output only for
+   // read command using inferred tri-state buffer
+   assign to_pi_ena = cmd[0]&cmd[1];
+   assign iop = to_pi_ena ? dout : 1'bz;
+   assign din = iop;
+
    // Output
    assign dout = sum_in[31];
    
    // Debug stuff
-   // assign leds[5:3] = a_out[2:0];
-   // assign leds[2:0] = b_out[2:0];
+   assign leds[5:3] = ~a_out[2:0];
+   assign leds[2:0] = ~b_out[2:0];
    //assign leds[5:3] = sum_out[2:0];
    //assign leds[2:0] = sum_in[2:0];
 
